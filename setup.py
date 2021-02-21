97f60017c3a4aa4ff0f61f0442ea3a53cf404f35
nipdep <nipun1deelaka@gmail.com>
2021-02-21 16:04:52 +0530

    Release: imgann-0.7.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 
     name='imgann',
 
-    version='0.5.4',
+    version='0.7.1',
 
     description='image annotation file operation provider.',
 
@@ -25,22 +25,22 @@ setup(
 
     author_email='nipun1deelaka@gmail.com',
 
-    classifiers=[  # Optional
-
-        'Development Status :: 3 - Alpha',
-
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
-    ],
+    # classifiers=[  # Optional
+    #
+    #     'Development Status :: 3 - Alpha',
+    #
+    #     'Intended Audience :: Developers',
+    #     'Topic :: Software Development :: Build Tools',
+    #
+    #     'License :: OSI Approved :: MIT License',
+    #
+    #     'Programming Language :: Python :: 3',
+    #     'Programming Language :: Python :: 3.5',
+    #     'Programming Language :: Python :: 3.6',
+    #     'Programming Language :: Python :: 3.7',
+    #     'Programming Language :: Python :: 3.8',
+    #     'Programming Language :: Python :: 3 :: Only',
+    # ],
 
 
     keywords='sample, setuptools, development',
