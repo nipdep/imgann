a8ddb9e87a0a19ff5e1d57c783f48577e7dee4de
nipun deelaka <46063855+nipdep@users.noreply.github.com>
2021-06-30 00:20:12 +0530

    bug fix ; coco2voc  closed #2

--- test/debug_convertors.py
+++ test/debug_convertors.py
@@ -4,12 +4,12 @@ from imgann import convert
 cnv = convert.Convertor()
 #%%
 # cnv.coco2csv('F:\JetBrain Project Files\Pycharm\security-point-object-detecton-Torch\data\data',
-#                     'F:\JetBrain Project Files\Pycharm\security-point-object-detecton-Torch\data\COCO\dataset.json',
-#                     '../data/data.csv')
+#              'F:\JetBrain Project Files\Pycharm\security-point-object-detecton-Torch\data\COCO\dataset.json',
+#              '../data/data.csv')
 
-# cnv.coco2voc('E:/JetBrain Project Files/Pycharm/security-point-object-detecton-Torch/data/data',
-#              '../data/voc2coco.json',
-#              '../data/voc')
+cnv.coco2voc('E:/JetBrain Project Files/Pycharm/security-point-object-detecton-Torch/data/data',
+             '../data/voc2coco.json',
+             '../data/voc', False)
 
 # cnv.csv2voc('F:\JetBrain Project Files\Pycharm\security-point-object-detecton-Torch\data\data',
 #               '../data/data.csv',
@@ -19,9 +19,9 @@ cnv = convert.Convertor()
 #                '../data/data.csv',
 #                '../data/csv2coco1.json')
 
-cnv.voc2coco('C:/Users/deela/Downloads/Hard Hat Sample.v5.voc/train',
-             'C:/Users/deela/Downloads/Hard Hat Sample.v5.voc/train_voc',
-             '../data/voc2cocohatt.json')
+# cnv.voc2coco('C:/Users/deela/Downloads/Hard Hat Sample.v5.voc/train',
+#              'C:/Users/deela/Downloads/Hard Hat Sample.v5.voc/train_voc',
+#              '../data/voc2cocohatt.json')
 
 # cnv.voc2csv('F:\JetBrain Project Files\Pycharm\security-point-object-detecton-Torch\data\data',
 #              '../data/voc',
